814f855e5d5fb277b067d03fa6380e0a3873a3f3
DwarfPorter <mrvlmor@gmail.com>
2022-04-17 20:15:51 +0300

    Initial commit

---
+++ app/src/main/java/ru/geekbrains/team_2022_04_17/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.geekbrains.team_2022_04_17
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
