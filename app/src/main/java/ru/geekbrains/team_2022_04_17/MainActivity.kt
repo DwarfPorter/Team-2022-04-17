c89f9afde506904b320ba11d7caf226a9d4c485f
DwarfPorter <mrvlmor@gmail.com>
2022-04-17 20:31:05 +0300

    кнопка

--- app/src/main/java/ru/geekbrains/team_2022_04_17/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/team_2022_04_17/MainActivity.kt
@@ -2,10 +2,18 @@ package ru.geekbrains.team_2022_04_17
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            Toast.makeText(this, "TEXT", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
