8b5095a109edd361691f3627979e8a990ca9a3fc
DwarfPorter <mrvlmor@gmail.com>
2022-04-17 20:33:14 +0300

    toast

--- app/src/main/java/ru/geekbrains/team_2022_04_17/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/team_2022_04_17/MainActivity.kt
@@ -2,10 +2,14 @@ package ru.geekbrains.team_2022_04_17
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show()
+
     }
 }
